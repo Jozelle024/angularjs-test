ea3db94ff734df6a2d24d67a67ecf538c2ffdade
Jozelle Lauzon <jozellel@DSGNB264.local>
2018-04-09 12:18:23 +0200

    controller

--- app.js
+++ app.js
@@ -8,3 +8,27 @@ angular.module('main').controller('mainController', function($scope){
     $scope.data = "this is the scope";
     $scope.data2 = "this is scope data2";
 });
+
+angular.module('invoice', []).controller('invoiceController', function invoiceController(){
+    this.qty = 1;
+    this.cost = 2;
+    this.inCurr = 'EUR';
+    this.currencies = ['USD', 'EUR', 'CNY'];
+    this.usdToForeign = {
+        USD: 1,
+        EUR: 0.74,
+        CNY: 6.09
+    }
+
+    this.total = function total(outCurr) {
+        return this.convertCurrency(this.qty * this.cost, this.inCurr, outCurr);
+    }; // total is a function that calls convert currency 
+
+    this.convertCurrency = function convertCurrency(amount, inCurr, outCurr) {
+        return amount * this.usdToForeignRates[outCurr] / this.usdToForeignRates[inCurr];
+    };  // a function that converts the currency from current currency - inCurr, to converted currency - outCurr
+
+    this.pay = function pay() {
+        window.alert('Thanks!');
+    };
+});
