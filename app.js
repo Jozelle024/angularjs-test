36bd62c85a74d2fc2c5cd17fb94bc6aeebc902ca
Jozelle Lauzon <jozellel@DSGNB264.local>
2018-04-09 11:50:11 +0200

    ng-model

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-angular.module('App', ['main']);
+angular.module('App', ['main']); //to create another module, [] array of dependencies
 angular.module('main', []);
-angular.module('main').controller('mainController', function($scope){
+angular.module('main').controller('mainController', function($scope){ 
+    // controller that controls the whole element with ng-controller="main controller"
     // controller body
+    var data = " this is a data";
+    var data2 = " this is another data";
+    $scope.data = "this is the scope";
+    $scope.data2 = "this is scope data2";
 });
