274e9fd4c099c3e65e2dc424dbf0189de06e8c32
Jozelle Lauzon <jozellel@DSGNB264.local>
2018-04-09 12:51:26 +0200

    factory = service

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-angular.module('App', ['main']); //to create another module, [] array of dependencies
+angular.module('App', ['main', 'invoice', 'finance']); //to create another module, [] array of dependencies
+
 angular.module('main', []);
 angular.module('main').controller('mainController', function($scope){ 
     // controller that controls the whole element with ng-controller="main controller"
@@ -9,26 +10,3 @@ angular.module('main').controller('mainController', function($scope){
     $scope.data2 = "this is scope data2";
 });
 
-angular.module('invoice', []).controller('invoiceController', function invoiceController(){
-    this.qty = 1;
-    this.cost = 2;
-    this.inCurr = 'EUR';
-    this.currencies = ['USD', 'EUR', 'CNY'];
-    this.usdToForeign = {
-        USD: 1,
-        EUR: 0.74,
-        CNY: 6.09
-    }
-
-    this.total = function total(outCurr) {
-        return this.convertCurrency(this.qty * this.cost, this.inCurr, outCurr);
-    }; // total is a function that calls convert currency 
-
-    this.convertCurrency = function convertCurrency(amount, inCurr, outCurr) {
-        return amount * this.usdToForeignRates[outCurr] / this.usdToForeignRates[inCurr];
-    };  // a function that converts the currency from current currency - inCurr, to converted currency - outCurr
-
-    this.pay = function pay() {
-        window.alert('Thanks!');
-    };
-});
---
+++ finance.js
@@ -0,0 +1,18 @@
+angular.module('finance', [])
+.factory('currencyConverter', function(){
+    var currencies = ['USD', 'EUR', 'CNY'];
+    var usdToForeignRates = {
+        USD: 1,
+        EUR: 0.74,
+        CNY: 6.09
+    };
+
+    var convert = function (amount, inCurr, outCurr) {
+        return amount * usdToForeignRates[outCurr] / usdToForeignRates[inCurr];
+    };  // a function that converts the currency from current currency - inCurr, to converted currency - outCurr
+
+    return {
+        currencies: currencies,
+        convert: convert
+    };
+});
